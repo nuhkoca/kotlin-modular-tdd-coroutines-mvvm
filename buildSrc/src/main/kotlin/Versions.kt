34cf343ccc758538f19fafc9595303e2da9d79c7
nuhkoca <nuhkocaa@gmail.com>
2019-09-03 12:10:41 +0300

    Update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ object Versions {
 
     // Gradle plugins
     const val gradle_plugin = "3.5.0"
-    const val kotlin_gradle_plugin = "1.3.41"
+    const val kotlin_gradle_plugin = "1.3.50"
 
     // UI
     const val appcompat = "1.1.0-beta01"
@@ -15,10 +15,10 @@ object Versions {
     // Networking
     const val retrofit = "2.6.1"
     const val okHttp = "4.1.0"
-    const val gson = "2.6.0"
+    const val gson = "2.6.1"
 
     // Coroutines
-    const val coroutines = "1.2.2"
+    const val coroutines = "1.3.0"
 
     // Lifecycle
     const val lifecycle = "2.2.0-alpha01"
@@ -38,7 +38,7 @@ object Versions {
     const val leakcanary = "1.6.3"
 
     // Static code analysis
-    const val detekt = "1.0.0"
+    const val detekt = "1.0.1"
     const val ben_manes = "0.22.0"
     const val ktlint = "8.2.0"
 
--- data/src/main/java/com/mobilemovement/kotlintvmaze/data/SeriesRemoteDataSource.kt
+++ data/src/main/java/com/mobilemovement/kotlintvmaze/data/SeriesRemoteDataSource.kt
@@ -2,6 +2,8 @@ package com.mobilemovement.kotlintvmaze.data
 
 import javax.inject.Inject
 
-class SeriesRemoteDataSource @Inject constructor(private val mazeService: MazeService) : DataSource {
-    override suspend fun searchSeriesAsync(query: String?): List<SeriesRaw> = mazeService.searchSeriesAsync(query)
+class SeriesRemoteDataSource @Inject constructor(private val mazeService: MazeService) :
+    DataSource {
+    override suspend fun searchSeriesAsync(query: String?): List<SeriesRaw> =
+        mazeService.searchSeriesAsync(query)
 }
