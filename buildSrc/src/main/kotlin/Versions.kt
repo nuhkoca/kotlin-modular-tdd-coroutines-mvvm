412b7071803e40b7b1fb077dec6c3d98b1b8118e
nuhkoca <nuhkocaa@gmail.com>
2019-09-15 21:10:24 +0300

    Update dependencies and gradle

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -8,17 +8,17 @@ object Versions {
     const val kotlin_gradle_plugin = "1.3.50"
 
     // UI
-    const val appcompat = "1.1.0-beta01"
+    const val appcompat = "1.1.0"
     const val material = "1.1.0-alpha07"
     const val constraint_layout = "2.0.0-beta2"
 
     // Networking
     const val retrofit = "2.6.1"
-    const val okHttp = "4.1.0"
+    const val okHttp = "4.2.0"
     const val gson = "2.6.1"
 
     // Coroutines
-    const val coroutines = "1.3.0"
+    const val coroutines = "1.3.1"
 
     // Lifecycle
     const val lifecycle = "2.2.0-alpha01"
