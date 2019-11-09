760440712120f273678b0e7350c86e83bb6790bb
nuhkoca <nuhkocaa@gmail.com>
2019-11-10 02:08:41 +0300

    Implement databinding delegation

--- base/src/main/java/com/mobilemovement/kotlintvmaze/base/BaseViewHolder.kt
+++ base/src/main/java/com/mobilemovement/kotlintvmaze/base/BaseViewHolder.kt
@@ -1,18 +1,13 @@
 package com.mobilemovement.kotlintvmaze.base
 
 import android.view.View
-import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
 abstract class BaseViewHolder<DB : ViewDataBinding, T : Any>(itemView: View) :
     ViewHolder(itemView) {
 
-    protected var dataBinding: DB? = null
-
-    init {
-        dataBinding = DataBindingUtil.getBinding(itemView)
-    }
+    protected val dataBinding: DB? by getBinding(itemView)
 
     abstract fun bindTo(item: T)
 }
---
+++ base/src/main/java/com/mobilemovement/kotlintvmaze/base/GetBinding.kt
@@ -0,0 +1,23 @@
+package com.mobilemovement.kotlintvmaze.base
+
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+class GetBinding<in V : ViewHolder, out DB : ViewDataBinding>(
+    private val itemView: View
+) : ReadOnlyProperty<V, DB?> {
+
+    private var value: DB? = null
+
+    override fun getValue(thisRef: V, property: KProperty<*>): DB? {
+        value = value ?: DataBindingUtil.getBinding(itemView)
+        return value
+    }
+}
+
+fun <V : ViewHolder, DB : ViewDataBinding> getBinding(itemView: View) =
+    GetBinding<V, DB>(itemView)
