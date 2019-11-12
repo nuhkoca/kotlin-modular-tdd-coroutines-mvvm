2bf7688cbfa97b179b98b95faa547c0f8ffb21c7
nuhkoca <nuhkocaa@gmail.com>
2019-11-12 17:52:40 +0300

    Update library versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     // Kotlin
-    const val core_ktx = "1.2.0-alpha02"
+    const val core_ktx = "1.2.0-beta02"
     const val android_annotation = "1.1.0"
 
     // Gradle plugins
@@ -9,8 +9,8 @@ object Versions {
 
     // UI
     const val appcompat = "1.1.0"
-    const val material = "1.1.0-alpha07"
-    const val constraint_layout = "2.0.0-beta2"
+    const val material = "1.2.0-alpha01"
+    const val constraint_layout = "2.0.0-beta3"
 
     // Networking
     const val retrofit = "2.6.2"
@@ -21,7 +21,7 @@ object Versions {
     const val coroutines = "1.3.2"
 
     // Lifecycle
-    const val lifecycle = "2.2.0-alpha01"
+    const val lifecycle = "2.2.0-rc02"
 
     // DI
     const val dagger = "2.25.2"
