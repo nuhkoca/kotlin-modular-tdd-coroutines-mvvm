82f5624f1cae6a21f5d4af017ed545eaa1aad710
nuhkoca <nuhkocaa@gmail.com>
2019-11-16 22:47:23 +0300

    Fix issues

--- app/src/main/java/com/mobilemovement/kotlintvmaze/vm/MazeViewModelFactory.kt
+++ app/src/main/java/com/mobilemovement/kotlintvmaze/vm/MazeViewModelFactory.kt
@@ -6,7 +6,7 @@ import javax.inject.Inject
 import javax.inject.Provider
 import javax.inject.Singleton
 
-@Suppress("UNCHECKED_CAST", "TooGenericExceptionThrown")
+@Suppress("UNCHECKED_CAST", "TooGenericExceptionThrown", "SimpleRedundantLet")
 @Singleton
 class MazeViewModelFactory @Inject constructor(
     private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
