1a6eda938cfdb62b1d1929fcd477dd72e5f76db5
nuhkoca <nuhkocaa@gmail.com>
2021-08-30 15:32:35 +0300

    Streamline Github Actions

--- checksum.sh
+++ checksum.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 RESULT_FILE=$1
 
-if [[ -f ${RESULT_FILE} ]]; then
-  rm ${RESULT_FILE}
+if [ -f $RESULT_FILE ]; then
+  rm $RESULT_FILE
 fi
-touch ${RESULT_FILE}
+touch $RESULT_FILE
 
 checksum_file() {
   echo $(openssl md5 $1 | awk '{print $2}')
@@ -14,12 +28,11 @@ checksum_file() {
 FILES=()
 while read -r -d ''; do
 	FILES+=("$REPLY")
-done < <(find . -type f \( -name "build.gradle*" -o -name "settings.gradle*" -o -name "gradle-wrapper.properties" \) -print0)
+done < <(find . -type f \( -name "build.gradle*" -o -name "Dependencies.kt" -o -name "gradle-wrapper.properties" \) -print0)
 
 # Loop through files and append MD5 to result file
 for FILE in ${FILES[@]}; do
-	echo $(checksum_file ${FILE}) >> ${RESULT_FILE}
+	echo $(checksum_file $FILE) >> $RESULT_FILE
 done
-# Now sort the file so that it is idempotent.
-sort ${RESULT_FILE} -o ${RESULT_FILE}
-
+# Now sort the file so that it is
+sort $RESULT_FILE -o $RESULT_FILE
